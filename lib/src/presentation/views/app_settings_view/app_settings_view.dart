19084412aa624aa49e07dfeac175103fcc318795
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2023-11-16 15:43:01 +0300

    settings-view-fix

--- lib/src/presentation/views/app_settings_view/app_settings_view.dart
+++ lib/src/presentation/views/app_settings_view/app_settings_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ionicons/ionicons.dart';
 import '../../../config/router/router.routes.dart';
 import '../../cubits/app_cubit/app_cubit.dart';
 import 'package:route_map/route_map.dart';
@@ -23,26 +24,28 @@ class AppSettingsView extends HookWidget {
               onTap: () {
                 //TODO : Uygulama Hakkında
               },
+              trailing: const Icon(Ionicons.chevron_forward),
             ),
             ListTile(
               title: const Text("Gizlilik Politikası"),
               onTap: () {
                 //TODO : Gizlilik Politikası
               },
+              trailing: const Icon(Ionicons.chevron_forward),
             ),
             ListTile(
               title: const Text("Kullanım Koşulları"),
               onTap: () {
                 //TODO : Kullanım Koşulları
               },
+              trailing: const Icon(Ionicons.chevron_forward),
             ),
             BlocBuilder<AppCubit, AppState>(
               builder: (context, state) {
                 return SwitchListTile.adaptive(
                   title: const Text("Karanlık Mod"),
                   value: appCubit.state.isDark,
-                  thumbColor: MaterialStateProperty.all(Colors.red),
-                  overlayColor: MaterialStateProperty.all(Colors.red),
+                  thumbColor: MaterialStateProperty.all(Colors.black),
                   activeColor: Colors.white,
                   inactiveTrackColor: Colors.grey.shade300,
                   activeTrackColor: Colors.red.shade300,
@@ -57,10 +60,34 @@ class AppSettingsView extends HookWidget {
                 return ListTile(
                   title: const Text("Çıkış Yap"),
                   onTap: () async {
-                    await appCubit.logout();
-                    LoginViewRoute()
-                        .pushAndRemoveUntil(context, (route) => false);
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            backgroundColor: Colors.white,
+                            title: const Text("Çıkış Yap"),
+                            content: const Text(
+                                "Çıkış yapmak istediğinize emin misiniz?"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("İptal"),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  await appCubit.logout();
+                                  LoginViewRoute().pushAndRemoveUntil(
+                                      context, (route) => false);
+                                },
+                                child: const Text("Çıkış Yap"),
+                              ),
+                            ],
+                          );
+                        });
                   },
+                  trailing: const Icon(Ionicons.chevron_forward),
                 );
               },
             ),
